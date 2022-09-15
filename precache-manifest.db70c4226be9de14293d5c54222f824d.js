7fa62f013896fa1b51c9688215de2c7c7889aa94
Jaloliddin Makhmudov <makhmudov@tuta.io>
2022-09-15 18:48:37 +0500

    Updates

---
+++ precache-manifest.db70c4226be9de14293d5c54222f824d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "121fcd2a509657388f63",
+    "url": "/antiBoring/static/css/main.e0a7473e.chunk.css"
+  },
+  {
+    "revision": "121fcd2a509657388f63",
+    "url": "/antiBoring/static/js/main.121fcd2a.chunk.js"
+  },
+  {
+    "revision": "48dc1c1c87d8faf3dbc4",
+    "url": "/antiBoring/static/js/1.48dc1c1c.chunk.js"
+  },
+  {
+    "revision": "eee9135b7ae2848cec65",
+    "url": "/antiBoring/static/js/runtime~main.eee9135b.js"
+  },
+  {
+    "revision": "df8e5217d2ea1c3136256f6f68d5fdb6",
+    "url": "/antiBoring/index.html"
+  }
+];
